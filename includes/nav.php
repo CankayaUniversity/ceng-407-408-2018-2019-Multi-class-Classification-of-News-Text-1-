86fdbed452c4ea7245eece777f53de5192a13cf1
Can Koral ADALI <c1411002@student.cankaya.edu.tr>
2019-03-13 11:06:39 +0300

    Navigation Menu Separate

---
+++ includes/nav.php
@@ -0,0 +1,79 @@
+<nav id="menu1" class="bar bar-1 hidden-xs bg--dark">
+                    <div class="container">
+                        <div class="row">
+                            <div class="col-lg-1 col-md-2 hidden-xs">
+                                <div class="bar__module">
+                                    <a href="index.html"> <img class="logo logo-light" alt="logo" src="img/logo-light.png"> </a>
+                                </div>
+                            </div>
+                            <div class="col-lg-11 col-md-12 text-right text-left-xs text-left-sm">
+                                <div class="bar__module">
+                                    <ul class="menu-horizontal text-left">
+                                        <li> <a href="#">
+                                        Single Link
+                                    </a> </li>
+                                        <li class="dropdown"> <span class="dropdown__trigger">
+                                        Dropdown Slim
+                                    </span>
+                                            <div class="dropdown__container">
+                                                <div class="container">
+                                                    <div class="row">
+                                                        <div class="dropdown__content col-lg-2">
+                                                            <ul class="menu-vertical">
+                                                                <li> <a href="#">Single Link</a> </li>
+                                                            </ul>
+                                                        </div>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        </li>
+                                        <li class="dropdown"> <span class="dropdown__trigger">
+                                        Dropdown Wide
+                                    </span>
+                                            <div class="dropdown__container">
+                                                <div class="container">
+                                                    <div class="row">
+                                                        <div class="dropdown__content row w-100">
+                                                            <div class="col-lg-3">
+                                                                <h5>Menu Title</h5>
+                                                                <ul class="menu-vertical">
+                                                                    <li> <a href="#">Single Link</a> </li>
+                                                                </ul>
+                                                            </div>
+                                                            <div class="col-lg-3">
+                                                                <h5>Menu Title</h5>
+                                                                <ul class="menu-vertical">
+                                                                    <li> <a href="#">Single Link</a> </li>
+                                                                </ul>
+                                                            </div>
+                                                            <div class="col-lg-3">
+                                                                <h5>Menu Title</h5>
+                                                                <ul class="menu-vertical">
+                                                                    <li> <a href="#">Single Link</a> </li>
+                                                                </ul>
+                                                            </div>
+                                                            <div class="col-lg-3">
+                                                                <h5>Menu Title</h5>
+                                                                <ul class="menu-vertical">
+                                                                    <li> <a href="#">Single Link</a> </li>
+                                                                </ul>
+                                                            </div>
+                                                        </div>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        </li>
+                                    </ul>
+                                </div>
+                                <div class="bar__module">
+                                    <a class="btn btn--sm type--uppercase" href="#customise-template"> <span class="btn__text">
+                                    Sign Up
+                                </span> </a>
+                                    <a class="btn btn--sm btn--primary type--uppercase" href="#purchase-template"> <span class="btn__text">
+                                    Login
+                                </span> </a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </nav>
