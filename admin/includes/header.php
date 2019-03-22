00c2ce7fdc6db0c94636b051fced48a3bdd8ffcb
Can Koral ADALI <c1411002@student.cankaya.edu.tr>
2019-03-22 10:26:55 +0300

    Admin Navigation

--- admin/includes/header.php
+++ admin/includes/header.php
@@ -17,4 +17,4 @@
         <link href="https://fonts.googleapis.com/css?family=Open+Sans:200,300,400,400i,500,600,700%7CMerriweather:300,300i" rel="stylesheet">
         <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
     </head>
-    <body>
+    <body class="dropdowns--hover">
--- admin/index.php
+++ admin/index.php
@@ -23,64 +23,11 @@ include "includes/header.php";
                 <!--end of container-->
             </div>
             <!--end bar-->
-            <nav class="bar bar--sm bg--dark" id="menu5">
-                <div class="container">
-                    <div class="row">
-                        <div class="col-lg-1 hidden-xs hidden-sm">
-                            <div class="bar__module">
-                                <a href="index.html">
-                                    <img class="logo logo-dark" alt="logo" src="img/logo-dark.png" />
-                                    <img class="logo logo-light" alt="logo" src="img/logo-light.png" />
-                                </a>
-                            </div>
-                            <!--end module-->
-                        </div>
-                        <div class="col-lg-5">
-                            <div class="bar__module">
-                                <ul class="menu-horizontal">
-                                    <li>
-                                        <a href="#">
-                                            <i class="stack-interface stack-plus-circled"></i> Create
-                                        </a>
-                                    </li>
-                                    <li>
-                                        <a href="#">
-                                            <i class="stack-interface stack-cog"></i> Manage
-                                        </a>
-                                    </li>
-                                </ul>
-                            </div>
-                        </div>
-                        <!--end columns-->
-                        <div class="col-lg-6 text-right text-left-xs">
-                            <div class="bar__module">
-                                <ul class="menu-horizontal">
-                                    <li class="dropdown">
-                                        <span class="dropdown__trigger">
-                                            <img alt="avatar" class="avatar image--xxs" src="img/avatar-round-1.png" /> Username
-                                        </span>
-                                    </li>
-                                    <li class="dropdown">
-                                        <span class="dropdown__trigger">
-                                            <i class="stack-interface stack-bell"></i> Alerts
-                                        </span>
-                                    </li>
-                                </ul>
-                            </div>
-                            <div class="bar__module">
-                                <a class="btn btn--primary btn--sm type--uppercase" href="#">
-                                    <span class="btn__text">
-                                        Upgrade
-                                    </span>
-                                </a>
-                            </div>
-                        </div>
-                        <!--end columns-->
-                    </div>
-                    <!--end of row-->
-                </div>
-                <!--end of container-->
-            </nav>
+
+<?php
+include "includes/nav.php";
+?>
+
             <section class="height-50 imagebg text-center" data-overlay="4">
                 <div class="background-image-holder">
                     <img alt="background" src="img/landing-13.jpg" /> <!--some background image-->
