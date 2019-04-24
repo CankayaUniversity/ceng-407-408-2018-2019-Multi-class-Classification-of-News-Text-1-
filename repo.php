59d1dae7dce3e4b4d56cc1e16affb0d3d5bda54e
Can Koral ADALI <c1411002@student.cankaya.edu.tr>
2019-04-24 12:01:31 +0300

    Repo Upload v1

--- repo.php
+++ repo.php
@@ -13,6 +13,34 @@
 		}
 	}
 ?>
+
+<?php
+   if(isset($_FILES['text'])){
+      $errors= array();
+      $file_name = $_FILES['text']['name'];
+      $file_size =$_FILES['text']['size'];
+      $file_tmp =$_FILES['text']['tmp_name'];
+      $file_type=$_FILES['text']['type'];
+      $file_ext=strtolower(end(explode('.',$_FILES['text']['name'])));
+
+      $extensions= array("txt","lab");
+
+      if(in_array($file_ext,$extensions)=== false){
+         $errors[]="File extension is not correct, please choose a txt or lab file.";
+      }
+
+      if($file_size > 2097152){
+         $errors[]='File size must be excately 2 MB';
+      }
+
+      if(empty($errors)==true){
+         move_uploaded_file($file_tmp,"files/".$file_name);
+         echo "Success";
+      }else{
+         print_r($errors);
+      }
+   }
+?>
 <!DOCTYPE html>
 <html>
 <head>
@@ -32,6 +60,7 @@
         <link href="css/custom.css" rel="stylesheet" type="text/css" media="all" />
         <link href="https://fonts.googleapis.com/css?family=Open+Sans:200,300,400,400i,500,600,700%7CMerriweather:300,300i" rel="stylesheet">
         <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+
 
 </head>
 <body>
@@ -61,7 +90,7 @@
 			<td><?php echo $row['repoName']; ?></td>
 			<td><?php echo $row['repoPath']; ?></td>
 			<td>
-				<a href="repo.php?edit=<?php echo $row['repoID']; ?>" class="edit_btn" >Edit</a>
+				<a href="repo.php?edit=<?php echo $row['repoID']; ?>" class="edit_btn" onclick="myFunction()">Edit</a>
                 &nbsp;
                 <a href="repo_control.php?del=<?php echo $row['repoID']; ?>" class="del_btn" >Delete</a>
 			</td>
@@ -74,6 +103,8 @@
 
 
 	<form method="post" action="repo_control.php" >
+<!--        class="hidden"-->
+<!--        <div id="a" style="display:none">-->
         <div class="input-group">
 
 			<input type="hidden" name="repoID" value="<?php echo $repoID; ?>">
@@ -95,6 +126,26 @@
 	           <button class="btn btn--primary" type="submit" name="save" >Save</button>
             <?php endif ?>
 		</div>
+<!--            </div>-->
 	</form>
+
+    <form action="" method="POST" enctype="multipart/form-data">
+         <input type="file" name="text" />
+         <input type="submit"/>
+      </form>
+
+
+
+        <!--<script>
+       function myFunction() {
+  var x = document.getElementById("a");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+}
+</script>-->
 </body>
+
 </html>
